8c78ae544f7061cbdaf02f732c2ebba623bc13d9
jiakai <gujiakai28@gmail.com>
2022-12-26 10:34:11 +0800

    delete fancybox cdn

--- static/js/custom.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $("img").each(function() {
-        var currentImage = $(this);
-        currentImage.wrap("<a href='" + currentImage.attr("src") + "' data-fancybox='gallery' data-caption='" + currentImage.attr("alt") + "'></a>");
-    });
-});
