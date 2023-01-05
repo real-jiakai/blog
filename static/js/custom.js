02325dc5f2374c78e1cade1f2ac38c547562f62b
jiakai <gujiakai28@gmail.com>
2023-01-05 11:55:24 +0800

    add lightbox

---
+++ static/js/custom.js
@@ -0,0 +1,13 @@
+const re = /.{32}(\w+).?(jpg|gif|png|webp)/
+
+function replacer(match,p1,p2){
+    return p1;
+}
+
+$(document).ready(function() {
+    $("img").each(function() {
+        var currentImage = $(this);
+        var uniq_name = currentImage.attr("src").replace(re,replacer)
+        currentImage.wrap("<a href='" + currentImage.attr("src") + "' data-lightbox='" + uniq_name + "' data-title='" + currentImage.attr("alt") + "'></a>");
+    });
+});
